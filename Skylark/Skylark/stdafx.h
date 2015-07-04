90a6b72679f3f9b00a07e2bb61865d8bc5c06a81
jwvg0425 <jwvg0425@naver.com>
2015-07-05 01:44:56 +0900

    pch

--- Skylark/Skylark/stdafx.h
+++ Skylark/Skylark/stdafx.h
@@ -9,6 +9,28 @@
 
 #define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용은 Windows 헤더에서 제외합니다.
 
+#include <process.h>
+#include <assert.h>
+#include <limits.h>
 
+#include <WinSock2.h>
+#include <MSWSock.h>
+
+#include <cstdint>
+#include <memory>
+#include <deque>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <type_traits>
+#include <iostream>
+#include <sstream>
+#include <functional>
+
+#include <ppl.h>
+#include <concurrent_queue.h>
 
 // TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
