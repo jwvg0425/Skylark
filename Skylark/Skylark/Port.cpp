a7edbf2e2e4b404da6fbb39440dfaf476516be57
jwvg0425 <jwvg0425@naver.com>
2015-07-05 21:16:10 +0900

    port ctor & dtor

--- Skylark/Skylark/Port.cpp
+++ Skylark/Skylark/Port.cpp
@@ -1,5 +1,17 @@
 #include "stdafx.h"
 #include "Port.h"
+#include "Exception.h"
+
+skylark::Port::Port()
+{
+	completionPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	CRASH_ASSERT(completionPort != NULL);
+}
+
+skylark::Port::~Port()
+{
+	CloseHandle(completionPort);
+}
 
 bool skylark::Port::bind(SOCKET socket)
 {
--- Skylark/Skylark/Port.h
+++ Skylark/Skylark/Port.h
@@ -6,6 +6,9 @@ namespace skylark
 	class Port
 	{
 	public:
+		Port();
+		~Port();
+
 		bool bind(SOCKET socket);
 	private:
 		HANDLE completionPort;
