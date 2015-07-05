3c5cf8c323b917b7344a1e3751ff3241ea5acd58
jwvg0425 <jwvg0425@naver.com>
2015-07-05 21:15:38 +0900

    remove unused TLS

--- Skylark/Skylark/ThreadLocal.cpp
+++ Skylark/Skylark/ThreadLocal.cpp
@@ -2,6 +2,4 @@
 #include "ThreadLocal.h"
 #include "LockOrderChecker.h"
 
-thread_local int skylark::TLS::threadType = -1;
-thread_local int skylark::TLS::workerThreadId = -1;
 thread_local skylark::LockOrderChecker* skylark::TLS::lockOrderChecker = nullptr;
--- Skylark/Skylark/ThreadLocal.h
+++ Skylark/Skylark/ThreadLocal.h
@@ -2,26 +2,10 @@
 
 namespace skylark
 {
-	enum class ThreadType
-	{
-		MAIN,
-		IO_WORKER,
-		DB_WORKER
-	};
-
-	enum COMPLETION_KEY_TYPE
-	{
-		NONE,
-		DB_REQUEST,
-		DB_RESULT
-	};
-
 	class LockOrderChecker;
 
 	namespace TLS
 	{
-		extern thread_local int threadType;
-		extern thread_local int workerThreadId;
 		extern thread_local LockOrderChecker* lockOrderChecker;
 	}
 }
