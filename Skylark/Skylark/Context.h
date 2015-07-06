8738877a13bedc8081db2dc69b6c6d7bbccf7693
jwvg0425 <jwvg0425@naver.com>
2015-07-06 11:31:09 +0900

    context bug fix

--- Skylark/Skylark/Context.h
+++ Skylark/Skylark/Context.h
@@ -4,16 +4,28 @@ namespace skylark
 {
 	struct Context
 	{
-		Context(int type_) : type(type_) {}
+		Context() = default;
 		~Context() = default;
 
 		virtual bool onComplete(int transferred, int key) = 0;
 		
 		//call when onComplete return false
 		virtual bool onFailure() = 0;
+	};
 
-		OVERLAPPED overlapped;
-		WSABUF wsabuf;
-		int type;
+	struct Overlapped : OVERLAPPED
+	{
+		Overlapped(Context* context_) : context(context_)
+		{
+			//init overlapped
+			memset(this, 0, sizeof(OVERLAPPED));
+		}
+
+		~Overlapped()
+		{
+			delete context;
+		}
+
+		Context* context;
 	};
 }
